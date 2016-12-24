88f2cb1e90e820df559573ea1dde2a7902b3ca0c
Welton Cardoso <weltonvcardoso@gmail.com>
2016-12-24 10:05:40 -0200

    Add files via upload

---
+++ gesfin/views/template.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="shortcut icon" href="logo.ico" type="image/x-icon" />
+        <title>GESFIN Sistema de Gestão Finaceira</title>
+    </head>
+    <body>
+        <?php
+        $this->loadViewInTemplate($viewName, $viewData);
+        ?>
+    </body>
+</html>
